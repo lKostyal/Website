22027d5ec3d18e24e6c122ceca47b8275219ec93
lKostyal <74778873+lKostyal@users.noreply.github.com>
2020-12-04 13:06:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__list').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+});
